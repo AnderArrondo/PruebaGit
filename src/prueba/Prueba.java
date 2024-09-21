cefc250f80bbd66491fa05e59c61742c24c1d55a
ander <ander.arrondo.e@opendeusto.es>
2024-09-21 18:24:38 +0200

    Carpeta instrucciones, modificación del main

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -3,5 +3,10 @@ package prueba;
 public class Prueba {
 	public static void main(String[] args) {
 		System.out.println("Probando Git/GitHub");
+		
+		// EJEMPLO
+		
+		// ARRONDO:
+		System.out.println("arrondo ha estado aquí");
 	}
 }
